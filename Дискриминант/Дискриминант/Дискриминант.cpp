78223e0892068e98129c78c979fbc8d6506e9925
vladmet1 <bckf2012@yandex.ru>
2020-10-06 21:51:00 +0300

    ДЗ

---
+++ Дискриминант/Дискриминант/Дискриминант.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c, x;
+	cout << "Введите a: ";
+	cin >> a;
+	cout << "Введите b: ";
+	cin >> b;
+	cout << "Введите c: ";
+	cin >> c;
+	if ((b * b - 4 * a * c) >= 0)
+	{
+		x = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
+		cout << "x1: " << x << endl;
+		x = (-1 * b - sqrt(b * b - 4 * a * c)) / (2 * a);
+		cout << "x2: " << x << endl;
+	}
+	else
+	{
+		cout << "Нет корней" << endl;
+	}
+	return 0;
+}
---
+++ Среднеезначение/Среднеезначение/Среднеезначение.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, d, srz;
+	cout << "Введите 4 числа" << endl;
+	cin >> a >> b >> c >> d;
+	srz = (a + b + c + d) / 4;
+	cout << "Среднее арифметическое: " << srz;
+}
