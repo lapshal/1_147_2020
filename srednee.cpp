afdbccb29630c214ae35cc32d9aacb8afae1ad7e
Alexey <37597550+Borshecs@users.noreply.github.com>
2020-10-06 20:59:40 +0300

    Привет!

---
+++ srednee.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+int main()
+{
+
+
+    std::puts("please enter 4 numbers");
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    float d = 0;
+
+    scanf_s("%f %f %f %f", &a, &b, &c, &d);
+    getchar();
+    float x = a + b + c + d;
+    float f = x / 4;
+    std::printf("solution: %f\n", f);
+}
